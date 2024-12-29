0516473ec691a4977e464bb1ac304e9bd5e3e0ec
Mike Somov <mikhail.somov@laniakea-continuum.com>
2024-12-29 18:05:23 +0300

    - Added Profile View Controller

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Mike Somov on 29.12.2024.
+//
+
+import Foundation
+import UIKit
+
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - @IBOutlet properties
+    
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var profileAvatarImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
