6892365d99f30be66d05e7c069950d5785570dcf
Mike Somov <mikhail.somov@laniakea-continuum.com>
2025-01-22 00:14:00 +0300

    - Added TapBar Controller - Added Navigation Controller - Added Single Image View

---
+++ ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Mike Somov on 16.01.2025.
+//
+
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
